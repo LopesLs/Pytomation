f1f187f2226efb755ed551507bbd48d445d3135d
LopesLs <97265556+LopesLs@users.noreply.github.com>
2023-12-04 19:59:54 -0300

    (refactor): more readable code

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pyautogui
 from automation import start_automation
-screenWidth, screenHeight = pyautogui.size()
 
-# The result bellow will be positive alwways
-ajustScreenWith = screenWidth - 1366 # Ajusting x-axis if the screez size is different.
-ajustScreenHeight = screenHeight - 768 # Ajusting y-axis if the screez size is different.
+# Get the current screen size
+current_screen_width, current_screen_height = pyautogui.size()
 
-# Start of automation
-start_automation(ajustScreenWith, ajustScreenHeight)
+# Define the screen size used during development
+DEVELOPMENT_SCREEN_SIZE = (1366, 768)
+
+# Calculate the adjustment needed for different screen sizes
+adjust_screen_width = current_screen_width - DEVELOPMENT_SCREEN_SIZE[0]
+adjust_screen_height = current_screen_height - DEVELOPMENT_SCREEN_SIZE[1]
+
+# Start the automation, passing in the necessary adjustments
+start_automation(adjust_screen_width, adjust_screen_height)
