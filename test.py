7e26bc63932e91133f09e0c4d2f25d2acbdb8a36
Carlos L. ✔ <97265556+LopesLs@users.noreply.github.com>
2024-02-03 10:07:11 -0300

    chore(test): Removing testing files

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-
-pyautogui.PAUSE = 0.8
-pyautogui.hotkey("win", "e")
-pyautogui.press("tab", presses=3, interval=0.5)
-pyautogui.hotkey("ctrl", "space")
-pyautogui.write("Painel de Controle\Hardware e Sons\Dispositivos e Impressoras", interval=0.05)
-pyautogui.press("enter")
